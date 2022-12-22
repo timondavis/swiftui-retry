81b429e25e50932f5ded5b6016b5237384d2f958
Timon Davis <timon.a.davis@gmail.com>
2022-12-22 13:27:03 -0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Timon Davis on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
